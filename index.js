4bc6d05f773ba871a8c0cbde802fa96333ff8fb7
eoriwech <prakash_gurung@hotmail.co.uk>
2023-10-11 16:18:28 +0100

    Feat: added function to play game
    Added function to play one round and loop that 5 time for single game

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
 function getComputerChoice() {
-    let choice = ["Rock", "Paper", "Scissors"];
+    let choice = ["Rock", "Paper", "Scissor"];
     let randomIndex = Math.floor(Math.random() * 3);
     return choice[randomIndex];
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
+    playerSelection.toLowerCase();
+    console.log(computerSelection)
+    if (playerSelection == "rock" && computerSelection == "Paper") {
+        return `You Lose! Paper beats Rock`
+    } else if (playerSelection == "rock" && computerSelection == "Scissor") {
+        return `You Win! Rock beats Scissor`
+    } else if (playerSelection == "paper" && computerSelection == "Scissor") {
+        return `You Lose! Scissor beats Paper`
+    } else if (playerSelection == "paper" && computerSelection == "Rock") {
+        return `You Win! Paper beats Rock`
+    } else if (playerSelection == "scissor" && computerSelection == "Rock") {
+            return `You Lose! Scissor beats Rock`
+    } else if (playerSelection == "scissor" && computerSelection == "Paper") {
+        return `You Win! Scissor beats Paper`
+    } else {
+        return `It is a Draw!`
+    }
+}
+
+function game() {
+    for (let i = 1; i < 6; i++) {
+        let playerSelection = prompt("Choose rock, paper or scissor");
+        console.log(playRound(playerSelection, getComputerChoice()));
+    }
+}
+
+game()
